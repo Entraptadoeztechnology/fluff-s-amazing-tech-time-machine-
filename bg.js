5f81228591be5be71feec61b29a1ab2650b8f690
Entraptadoeztech <272025446+entraptadoeztech@users.noreply.github.com>
2026-07-08 20:47:28 -0500

    Update bg.js

--- bg.js
+++ bg.js
@@ -7,7 +7,7 @@ window.onload = () => {
     let scrollX = 0;
 
     setInterval(() => {
-        scrollX += 5;
+        scrollX += 50;
         if (scrollX > 2300) scrollX = 0;
         // console.log(scrollX);
         document.querySelector(".header").style = `background-position: -${scrollX}px bottom;`;
